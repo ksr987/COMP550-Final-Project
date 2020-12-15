5c04486481bbea5b380c4f51fada4d008d0b79e3
ksr987 <rampurkarketan@gmail.com>
2020-12-15 18:34:14 -0500

    added ngrams file

---
+++ source/ngrams.py
@@ -0,0 +1,19 @@
+from load import load_preprocessed_data
+from nltk import ngrams
+
+train,dev,test = load_preprocessed_data()
+train_copy = train.copy()
+
+def compute_text_ngram(data, n):
+    for subreddit in data:
+        for i, post in enumerate(data[subreddit]):
+            data[subreddit][i] = (post[0], list(ngrams(post[1], n)), post[2])
+    return data
+
+def main():
+    ngram_data = compute_text_ngram(train_copy, 6)
+    #print first 5 posts of Advice subreddit
+    print(ngram_data['Advice'][:5])
+
+if __name__ == '__main__':
+    main()
